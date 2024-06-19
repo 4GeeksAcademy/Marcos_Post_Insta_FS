e7eba11c9417f0a238fe9d2f5810b8fe9e0d799d
MarcosEstPam <marcosepamias@gmail.com>
2024-06-19 14:43:35 +0000

    V2

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
             button.classList.toggle("megusta-activo");
         });
     });
-});
-
-//document.addEventListener("DOMContentLoaded", (event)=>{
-  //  const botonguardar = document.querySelectorAll (".guardar i");
-    //    botonguardar.forEach (button => {
-      //      button.addEventListener("click",()=> {
-        //        button.classList.toggle("fa-regular");
-            //    button.classList.toggle("fa-solid");
-          //      button.classList.toggle("guardar-activo");
+    const botonguardar = document.querySelectorAll (".guardar i");
+       botonguardar.forEach (button => {
+            button.addEventListener("click",()=> {
+                button.classList.toggle("fa-regular");
+                button.classList.toggle("fa-solid");
+                button.classList.toggle("guardar-activo");
             });
         });
-};
+});
+
+
+    
